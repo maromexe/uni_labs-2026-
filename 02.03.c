a3adb997d07f9aa85f73e87fadcd4a61376c8fb6
maromexe <mariyka.rmk@gmail.com>
2026-03-02 19:06:56 +0200

    processing

--- 02.03.c
+++ 02.03.c
@@ -6,52 +6,31 @@ int main() {
 
     int n, k = 0, s = 0;
 
-    printf("Enter n: ");
     scanf("%d", &n);
 
     float sn = sqrt(n);
-    printf("%f",sn);
 
-    while ((int)sn != sn){ /*пробдєма тут*/
-
+    while ((int)sn != sn){
         n--;
         k++;
         sn = sqrt(n);
-
-        printf("\nSn: %f\nSn: %d", sn, (int)sn);
-        printf("\nn: %d", n);
-        printf("\nk: %d", k);
     }
 
-
-
-    for (int i = 1; i <= sn; i++) {
+    for (int i = 1; i < sn + 1; i++) {
         s += i * 4;
     }
 
-    printf("\nRes %d", s);
-
-    for (int i = 1; i <= k; i++) {
-        printf("\ni: %d", i);
-        printf("\nSn: %d", (int)sn);
-        printf("\nk: %d", k);
-        if (i % (int)sn == 0) {
+    for (int i = 1; i < (k + 1); i++) {
+        if ((k % (int)sn == 0)){
             s += 3;
-            k--;
-            printf("\nS=%d",s);
         }
-
-        else if ((k % (int)sn > 0 && (k % (int)sn <= k)) ){
+        else {
             s += 2;
-            k--;
-            printf("\nS1=%d",s);
         }
-
-        printf("\n");
     }
 
 
-    printf("\nRes %d", s);
+    printf("%d", s);
 
 
     return 0;
