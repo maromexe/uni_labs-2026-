5d002a4f4b5886aa539caee6bb9060ea8d2ec382
maromexe <mariyka.rmk@gmail.com>
2026-04-15 01:22:12 +0300

    lab_2_2 fixed

--- LAB_2_2.c
+++ LAB_2_2.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 
-float h(int n, float s) {
-    if (n == 1) {
-        return s;
+void heritage(int current_gen, int total_gens, float money) {
+  if (current_gen <= total_gens) {
+    if (money < 10.0) {
+      int left_without = total_gens - current_gen + 1;
+      printf("Спадку вистачило тільки до %d-го покоління.\n", current_gen - 1);
+      printf("%d поколінь залишились без спадку.\n", left_without);
     }
-    return 0.5 * h(n - 1, s);
-}
-
-void print_h(int n, float s) {
-    for (int i = 1; i <= n; i++) {
-        printf("Amount of heritage %d generation will get: %.2f$\n", i, h(i, s));
+    else {
+      printf("Покоління %d отримає: %.2f$\n", current_gen, money);
+      heritage(current_gen + 1, total_gens, money / 2.0);
     }
+  }
 }
 
 int main() {
-    int n;
-    float s;
+  int n;
+  float s;
 
-    do {
-        printf("Enter a number of generations and amount of heritage: ");
-        scanf("%d%f", &n, &s);
-    }while (n <= 0 || s <= 0);
+  do {
+    printf("Введіть кількість поколінь та суму спадку: ");
+    scanf("%d%f", &n, &s);
+  } while (n <= 0 || s <= 0);
 
-    print_h(n, s);
+  printf("\nРозподіл спадку\n");
+  heritage(1, n, s);
 
-    return 0;
+  return 0;
 }
